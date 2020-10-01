936894dc4ebed5c669f6f92c11eb96ad5e0f6c57
ShaoqinHou <hwj0959@autuni.ac.nz>
2020-10-02 11:05:02 +1300

    after build

--- routes/api.js
+++ routes/api.js
@@ -6,42 +6,26 @@ const BlogPost = require('../models/blogPost');
 
 
 // Routes
-router.get('/', (req, res) => {
 
-    BlogPost.find({  })
-        .then((data) => {
-            console.log('Data: ', data);
-            res.json(data);
-        })
-        .catch((error) => {
-            console.log('error: ', daerrorta);
-        });
+//get all
+router.get('/', async(req, res, next) => {
+    try {
+        const result = await newBlogPost.find({},{__v:0});
+        res.send(result);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
-router.post('/save', (req, res) => {
-    const data = req.body;
-
-    const newBlogPost = new BlogPost(data);
-
-    newBlogPost.save((error) => {
-        if (error) {
-            res.status(500).json({ msg: 'Sorry, internal server errors' });
-            return;
-        }
-        // BlogPost
-        return res.json({
-            msg: 'Your data has been saved!!!!!!'
-        });
-    });
-});
-
-
-router.get('/name', (req, res) => {
-    const data =  {
-        username: 'peterson',
-        age: 5
-    };
-    res.json(data);
+//create new
+router.post('/', async (req, res, next) => {
+    try {
+        const newBlogPost = new BlogPost(req.body);
+        const result = await newBlogPost.save();
+        res.send(result);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 
