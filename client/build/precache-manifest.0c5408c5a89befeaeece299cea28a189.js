aa34cce3ebd6b9b8d7e7ad8ec77f5e68b725c60f
ShaoqinHou <hwj0959@autuni.ac.nz>
2020-10-07 12:00:27 +1300

    temp force heroku rebuild
    tset

---
+++ client/build/precache-manifest.0c5408c5a89befeaeece299cea28a189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faa08b2f6c51f623adaa0ae745e8f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82c88c309bb097d3d6be",
+    "url": "/static/css/main.879909f4.chunk.css"
+  },
+  {
+    "revision": "1d37735607c0f3891fae",
+    "url": "/static/js/2.5dc2da25.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/static/js/2.5dc2da25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c88c309bb097d3d6be",
+    "url": "/static/js/main.e445d78a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
